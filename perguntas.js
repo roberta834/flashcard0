ff4fa390ccca4c5e862ea373ac3c8b636cc912cd
roberta834 <gabriel.rocha.silva19@escola.pr.gov.br>
2025-08-19 13:46:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 3+9x3?',
+    '36'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica o Brasil?',
+    'O Brasil fica na america do sul'
+)
+
+criaCartao(
+    'Artes',
+    'Se misturar azul e vermelho que cor vai virar?',
+    'A mistura de azul com vermelho vira roxo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz I love my family and ny dog, my cat em Português?',
+    'Se diz Eu amo minha família e meu cachorro, e meu gato'
+)
